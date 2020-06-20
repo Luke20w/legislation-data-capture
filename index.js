cfbb75ad82249b11fc519a358242c4729ca831a8
Luke20w <weidnerlule@gmail.com>
2020-06-20 16:22:46 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const pupeteer = require("puppeteer");
+const crawler = require("crawler-request");
+
+async function scrapeIntroducedBills() {
+  const bills = [];
+
+  // Open the Ohio legislature page in a headless browser
+  const browser = await pupeteer.launch({
+    headless: true,
+  });
+  const page = await browser.newPage();
+  const url =
+    "https://www.legislature.ohio.gov/legislation/search?generalAssemblies=133&pageSize=500&sort=Number&start=1&subjects=21";
+  await page.goto(url);
+  console.log("browser opened...");
+
+  // Get the total number of bills in the table
+  const rowCount = await page.$$eval("tr", (trs) => trs.length - 1);
+
+  for (i = 0; i < rowCount; i++) {
+    // Get the status of the bill
+    const status = await page.$eval(
+      "tr:nth-child(" + (i + 2) + ") > td[class=statusCell] > span",
+      (span) => {
+        return span.innerText;
+      }
+    );
+
+    // Check if the bill is newly introduced or reported
+    if (status == "As Introduced" || status.includes("As Reported")) {
+      // Get the name of the bill
+      const name = await page.$eval(
+        "tr:nth-child(" + (i + 2) + ") > td[class=legislationCell] > a > span",
+        (span) => {
+          return span.innerText;
+        }
+      );
+
+      // Check if the bill is not already in the array
+      if (!bills.some((bill) => bill.name === name)) {
+        console.log("Parsing " + name + "...");
+
+        // Go to the bill page cooresponding to the index
+        await page.click(
+          "tr:nth-child(" + (i + 2) + ") > td[class=legislationCell] > a"
+        );
+
+        // Wait for the pdf link button to load in
+        await page.waitFor(".linkButton");
+
+        // Get the link to the bill pdf
+        const pdfLink = await page.$eval(".linkButton", (a) => a.href);
+
+        // Convert the pdf to a string and add it to the array of bills
+        crawler(pdfLink).then((response) => {
+          bills.push({ name: name, content: response.text });
+        });
+
+        // Go back to the table page
+        await page.goBack();
+      }
+    }
+  }
+
+  // console.log(bills);
+
+  console.log("closing browser...");
+  browser.close();
+}
+
+scrapeIntroducedBills();
