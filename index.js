9126a6d343f89d08971b5bbb2b9784b61d8d5b6a
Luke20w <weidnerluke@gmail.com>
2020-06-21 20:20:11 -0400

    v1.0.4

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const pupeteer = require("puppeteer");
 const crawler = require("crawler-request");
 const fs = require("fs");
+var FlexSearch = require("flexsearch");
 
 const bills = require("./bills.json");
+
+var index = new FlexSearch("score");
 
 async function scrapeNewBills() {
   // Open the Ohio legislature page in a headless browser
@@ -20,7 +23,15 @@ async function scrapeNewBills() {
   // Get the total number of bills in the table
   const rowCount = await page.$$eval("tr", (trs) => trs.length - 1);
 
-  for (i = 0; i < rowCount; i++) {
+  for (i = 0; i < 8; i++) {
+    // Get the name of the bill
+    const name = await page.$eval(
+      "tr:nth-child(" + (i + 2) + ") > td[class=legislationCell] > a > span",
+      (span) => {
+        return span.innerText;
+      }
+    );
+
     // Get the status of the bill
     const status = await page.$eval(
       "tr:nth-child(" + (i + 2) + ") > td[class=statusCell] > span",
@@ -31,14 +42,6 @@ async function scrapeNewBills() {
 
     // Check if the bill is newly introduced or reported
     if (status == "As Introduced" || status.includes("As Reported")) {
-      // Get the name of the bill
-      const name = await page.$eval(
-        "tr:nth-child(" + (i + 2) + ") > td[class=legislationCell] > a > span",
-        (span) => {
-          return span.innerText;
-        }
-      );
-
       // Get the title of the bill
       const title = await page.$eval(
         "tr:nth-child(" + (i + 2) + ") > td[class=titleCell] > span",
@@ -62,13 +65,66 @@ async function scrapeNewBills() {
         // Get the link to the bill pdf
         const pdfLink = await page.$eval(".linkButton", (a) => a.href);
 
-        // Convert the pdf to a string and add it to the array of bills
-        crawler(pdfLink).then((response) => {
-          bills.push({ name: name, title: title, content: response.text });
+        // Convert the pdf to a string
+        const rawContent = await crawler(pdfLink).then(
+          (response) => response.text
+        );
+
+        // Split the raw content into an array of its lines
+        const rawLines = rawContent.split(/\r\n|\r|\n/);
+
+        // Filter the lines array to remove bloat from the original pdf
+        const lines = rawLines.filter((line) => {
+          return !(
+            !isNaN(line) ||
+            line.includes("As Introduced") ||
+            line.includes("Page ") ||
+            line.includes(name)
+          );
         });
+
+        // Put the array of lines back together into one string
+        let content = "";
+        for (const line of lines) {
+          content += line + "\n";
+        }
+
+        // Split the filtered content into seperate sections
+        let sections = content.split("Sec. ");
+        for (i = 0; i < sections.length; i++) {
+          const sectionWords = sections[i].split(" ");
+          sections[i] = {
+            name: "Sec." + " " + sectionWords[0],
+            content: "Sec. " + sections[i],
+          };
+        }
+
+        // Add the bill object to the array
+        bills.push({ name: name, title: title, sections: sections });
+
+        // Add each section to the search index
+        for (s = 0; s < bills[bills.length - 1].sections.length; s++) {
+          console.log(
+            "Adding Bill index " +
+              (bills.length - 1) +
+              ", Section index " +
+              s +
+              " to the search index..."
+          );
+          index.add(
+            { bill: bills.length - 1, section: s },
+            bills[bills.length - 1].sections[s].content
+          );
+        }
 
         // Go back to the table page
         await page.goBack();
+      }
+    } else {
+      // Remove any bills in the array that have been passed
+      const index = bills.findIndex((bill) => bill.name === name);
+      if (index != -1) {
+        bills.splice(index);
       }
     }
   }
@@ -81,3 +137,5 @@ async function scrapeNewBills() {
 }
 
 scrapeNewBills();
+
+// console.log(index.search("graduation requirements"));
